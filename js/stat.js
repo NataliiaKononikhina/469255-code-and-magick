c972a19db8fddf4f966e938e9485ce901e51aa68
Nataliia Kononikhina <kononikhinan@gmail.com>
2018-01-31 08:28:53 +0100

    Изменила возможность указания fillStyle

--- js/stat.js
+++ js/stat.js
@@ -41,9 +41,9 @@ var getColor = function (red, green, blue, opacity) {
   return 'rgba(' + red + ',' + green + ',' + blue + ',' + opacity + ')';
 };
 
-var writeText = function (ctx, color, text, coordinateX, coordinateY) {
+var writeText = function (ctx, text, coordinateX, coordinateY, color) {
   ctx.font = '16px PT Mono';
-  ctx.fillStyle = color;
+  ctx.fillStyle = color || '#000';
   ctx.fillText(text, coordinateX, coordinateY);
 };
 
@@ -51,8 +51,8 @@ window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
-  writeText(ctx, '#000', 'Ура вы победили!', CLOUD_X + TEXT_PADDING_X, CLOUD_Y + PADDING_TOP);
-  writeText(ctx, '#000', 'Список результатов:', CLOUD_X + TEXT_PADDING_X, CLOUD_Y + PADDING_TOP + FONT_HEIGHT + CLOUD_GAP);
+  writeText(ctx, 'Ура вы победили!', CLOUD_X + TEXT_PADDING_X, CLOUD_Y + PADDING_TOP);
+  writeText(ctx, 'Список результатов:', CLOUD_X + TEXT_PADDING_X, CLOUD_Y + PADDING_TOP + FONT_HEIGHT + CLOUD_GAP);
 
   var maxTime = getMaxElement(times);
 
@@ -63,9 +63,24 @@ window.renderStatistics = function (ctx, names, times) {
       ctx.fillStyle = getColor(0, 0, 255, getRandomNumber(0.2, 1));
     }
 
-    ctx.fillRect(CLOUD_X + TEXT_PADDING_X + FONT_GAP + (BAR_WIDTH + BAR_GAP) * i, NAME_Y - CLOUD_GAP - FONT_HEIGHT, BAR_WIDTH, -(BAR_HEIGHT * times[i]) / maxTime);
+    ctx.fillRect(
+      CLOUD_X + TEXT_PADDING_X + FONT_GAP + (BAR_WIDTH + BAR_GAP) * i,
+      NAME_Y - CLOUD_GAP - FONT_HEIGHT,
+      BAR_WIDTH,
+      -(BAR_HEIGHT * times[i]) / maxTime
+    );
 
-    writeText(ctx, '#000', Math.round(times[i]), CLOUD_X + TEXT_PADDING_X + FONT_GAP + (BAR_WIDTH + BAR_GAP) * i, PADDING_TIME_TEXT);
-    writeText(ctx, '#000', names[i], CLOUD_X + TEXT_PADDING_X + FONT_GAP + (BAR_WIDTH + BAR_GAP) * i, NAME_Y);
+    writeText(
+      ctx,
+      Math.round(times[i]),
+      CLOUD_X + TEXT_PADDING_X + FONT_GAP + (BAR_WIDTH + BAR_GAP) * i,
+      PADDING_TIME_TEXT
+    );
+    writeText(
+      ctx,
+      names[i],
+      CLOUD_X + TEXT_PADDING_X + FONT_GAP + (BAR_WIDTH + BAR_GAP) * i,
+      NAME_Y
+    );
   }
 };
